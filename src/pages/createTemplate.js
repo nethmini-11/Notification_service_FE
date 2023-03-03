1bd72b644a4c7e9e85f6a7edcd1a4aed9f669bfc
Nethmini__0_11 <manthikaoriginal@gmail.com>
2023-03-03 11:42:19 +0530

    Update createTemplate.js

--- src/pages/createTemplate.js
+++ src/pages/createTemplate.js
@@ -6,11 +6,36 @@ import Swal from "sweetalert2";
 import { PostMailTemplate } from "../request/PostRequests";
 
 const CreateTemplate = () => {
-  //const [readOnly, setReadOnly] = useState(true);
+  
   const [templateName, setTemplateName] = useState("");
   const [subjectName, setSubjectName] = useState("");
   const [data, setData] = useState ("<html > <top-banner-advertisement> <h4>Hey <non-editable><receiver-name></non-editable>,</h4> <p>Thank you for placing a rental request on Cloud of Goods. We're waiting for one of our partners to accept your order. You will hear back from us as soon as the order is accepted. In the meantime, please review the details below and let us know of any changes. Please note that your credit card will not be charged until the order is accepted.</p> <h2>Order Summary Order #<non-editable><order-no></non-editable></h2> <non-editable><item-details-section></non-editable> <h3>Delivery Date: <non-editable><delivery-date></non-editable></h3> <h3>Delivery Time: <non-editable><delivery-time></non-editable></h3> <h3>Delivery Location: <non-editable><delivery-location-address></non-editable></h3> <h3>Pickup Date: <non-editable><pick-up-date></non-editable></h3> <h3>Pickup Time: <non-editable><pick-up-time></non-editable></h3> <h3>Pickup Location: <non-editable><pickup-location-address></non-editable></h3> <h2>Price Summary</h2> <h5>Total rental amount <non-editable><total-rental-amount></non-editable></h5> <h5>Damage Waiver <non-editable><damage-waiver-amount></non-editable></h5> <h4>Total paid <non-editable><total-paid-amount></non-editable></h4> <p>Please thoroughly review the above reservation details. If you have any questions or would like to modify the order, please reply directly to this email.</p> <p>* This does not happen often, but if the requested delivery times are already taken, <non-editable><vendor-name></non-editable> will give you a call to make adjustments.</p> <h2>Rules & Responsibilities</h2> <h4>● All items are carefully inspected before and after rentals. Any damage upon return is subject to additional fees. </h4> <h4>● You may reschedule delivery or change the order details only if <non-editable><vendor-name></non-editable> allows such options.</h4> <h4>● You may cancel or modify this order by logging into your Cloud of Goods account or sending an email to info@cloudofgoods.com with the subject: [CANCEL] <order-no> or [Modify] <order-no>. All cancellations are subject to our cancellation policy.</h4> <h4>We’ll see you then, cheers!</h4></br> - <non-editable><mail-send-by></non-editable> </html>");
   const postMailTemplateUrl = PostMailTemplate();
+
+ 
+  
+  
+
+  // const handleButton = () => {
+  //  
+  // 
+  //   if (!templateName || !subjectName || !data) {
+  //     Swal.fire({
+  //       icon: "error",
+  //       title: "Oops...",
+  //       text: "Please fill out all fields!",
+  //       timer: 10000, // set the alert showing time 
+  //     });
+  //   } else {
+  //     Swal.fire({
+  //       icon: "success",
+  //       title: "Template Created Successfully !!",
+  //       text: "🙂",
+  //       timer: 10000, // set the alert showing time 
+  //     });
+  //   }
+  // 
+  // };
 
   const handleButton = () => {
     if (!templateName || !subjectName || !data) {
@@ -18,11 +43,22 @@ const CreateTemplate = () => {
         icon: "error",
         title: "Oops...",
         text: "Please fill out all fields!",
+        allowOutsideClick: false, // prevent the user from clicking outside the alert box
+        allowEscapeKey: false, // prevent the user from dismissing the alert using the Escape key
+        timer: 2000, // set the alert showing time
       });
     } else {
-      Swal.fire("Good job!", "You clicked the button!", "success");
+      Swal.fire({
+        icon: "success",
+        title: "Template Created Successfully !!",
+        text: "🙂",
+        allowOutsideClick: false, // prevent the user from clicking outside the alert box
+        allowEscapeKey: false, // prevent the user from dismissing the alert using the Escape key
+        timer: 10000, // set the alert showing time
+      });
     }
   };
+  
   function postMailTemplate() {
     axios.post(postMailTemplateUrl, {
       editedEmailContent: data,
@@ -30,8 +66,6 @@ const CreateTemplate = () => {
       templateName: templateName,
     });
   }
-
-  
   
   return (
     <div>
@@ -121,8 +155,9 @@ const CreateTemplate = () => {
                               class="btn btn-send  pt-2 btn-block"
                               value="Create Template ✅"
                               onClick={(e) => {
-                                postMailTemplate();
+                               
                                 handleButton();
+                                postMailTemplate();
                               }}
                             />
                           </div>
