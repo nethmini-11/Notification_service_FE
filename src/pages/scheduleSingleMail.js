16f74cffbab978e926241925b829e56adf1eb685
Nethmini__0_11 <manthikaoriginal@gmail.com>
2023-03-31 15:20:14 +0530

    Update scheduleSingleMail.js

--- src/pages/scheduleSingleMail.js
+++ src/pages/scheduleSingleMail.js
@@ -1,13 +1,93 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import TimePicker from "react-time-picker";
-import { PostSchedularMail } from "../request/PostRequests"; 
+import DateTimePicker from "react-datetime-picker";
+import axios from "axios";
+import { PostSchedularMail } from "../request/PostRequests";
 import "react-datepicker/dist/react-datepicker.css";
+import Swal from "sweetalert2";
 
 const ScheduleSingleMail = () => {
-  const [startDate, setStartDate] = useState(new Date());
- 
-  const [value, onChange] = useState("10:00");
+  const [receiverEmail, setReceiverEmail] = useState("");
+  const [dateTime, setDateTime] = useState("");
+  const [title, setTitle] = useState("");
+  const [productName, setProductName] = useState("");
+  const [productQuantity, setProductQuantity] = useState("");
+  const [productUrl, setProductUrl] = useState("");
+  const [bannerUrl, setBannerUrl] = useState("");
+  const [templateName, setTemplateName] = useState("");
+  const [value, onChange] = useState(new Date());
+  const postSchedularMailUrl = PostSchedularMail();
+
+  const handleDateTimeChange = (newValue) => {
+    setDateTime(newValue.toISOString());
+  };
+
+  const handleButton = async (e) => {
+    // Check if all necessary fields are filled out
+    if (
+      !productName ||
+      !productQuantity ||
+      !productUrl ||
+      !receiverEmail ||
+      !bannerUrl ||
+      !title ||
+      !templateName ||
+      !dateTime
+    ) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Please fill out all fields!",
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        timer: 4000,
+      });
+    } else {
+      // If all fields are filled out, call postMailTemplate function
+      await postSchedularMail();
+    }
+  };
+  // Function that makes a POST request to server to send a new file email
+  function postSchedularMail() {
+    // Use axios library to make POST request to server
+    axios
+      .post(postSchedularMailUrl, {
+        dateTime: dateTime.toISOString(),
+        email: receiverEmail,
+        isActive: true,
+        mailContentDto: {
+          content: {},
+          ordersList: [
+            {
+              productName: productName,
+              productQuantity: productQuantity,
+              productUrl: productUrl,
+            },
+          ],
+
+          templateName: templateName,
+          topBannerAdUrl: bannerUrl,
+        },
+        subject: title,
+      })
+      .then((res) => {
+        console.log(res);
+        const { data, status } = res;
+        if (status === 200) {
+          // If request is successful, display success message using SweetAlert2 library
+          Swal.fire({
+            icon: "success",
+            title: data,
+            text: "🙂",
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            timer: 4000,
+          });
+        }
+      });
+  }
+
   return (
     <div>
       <div className="report">
@@ -43,51 +123,31 @@ const ScheduleSingleMail = () => {
                               <label for="form_name">Receiver's Email *</label>
                               <input
                                 id="form_name"
-                                type="text"
+                                type="email"
                                 name="name"
                                 class="form-control"
                                 placeholder="example@gmail.com *"
                                 required="required"
                                 data-error="Email is required."
+                                onChange={(e) => {
+                                  setReceiverEmail(e.target.value);
+                                }}
                               />
                             </div>
                           </div>
                           <div class="col-md-6">
                             <div class="form-group">
-                              <label for="form_lastname">
-                                Receiver's Mobile Number *
-                              </label>
-                              <input
-                                id="form_lastname"
-                                type="text"
-                                name="surname"
-                                class="form-control"
-                                placeholder="+941111111 *"
-                                required="required"
-                                data-error="Mobile number is required."
-                              />
+                              <label for="form_email">Select Date *</label>
+                              <div class="form-control">
+                                <DateTimePicker
+                                  onChange={setDateTime}
+                                  value={dateTime}
+                                />
+                              </div>
                             </div>
                           </div>
                         </div>
                         <div class="row">
-                          <div class="col-md-6">
-                            <div class="form-group">
-                              <label for="form_email">User Id *</label>
-                              <select
-                                id="form_need"
-                                name="need"
-                                class="form-control"
-                                required="required"
-                                data-error="Please specify your need."
-                              >
-                                <option value="" selected disabled>
-                                  --Select--
-                                </option>
-                                <option>100</option>
-                                <option>200</option>
-                              </select>
-                            </div>
-                          </div>
                           <div class="col-md-6">
                             <div class="form-group">
                               <label for="form_need">
@@ -99,6 +159,9 @@ const ScheduleSingleMail = () => {
                                 class="form-control"
                                 required="required"
                                 data-error="Template name is required."
+                                onChange={(e) => {
+                                  setTemplateName(e.target.value);
+                                }}
                               >
                                 <option value="" selected disabled>
                                   --Select--
@@ -109,6 +172,23 @@ const ScheduleSingleMail = () => {
                               </select>
                             </div>
                           </div>
+                          <div class="col-md-6">
+                            <div class="form-group">
+                              <label for="form_email">Mail Title *</label>
+                              <input
+                                id="form_email"
+                                type="text"
+                                name="email"
+                                class="form-control"
+                                placeholder="Lightweight Mobility  *"
+                                required="required"
+                                data-error="Product name is required."
+                                onChange={(e) => {
+                                  setTitle(e.target.value);
+                                }}
+                              />
+                            </div>
+                          </div>
                         </div>
                         <div class="row">
                           <div class="col-md-6">
@@ -116,12 +196,15 @@ const ScheduleSingleMail = () => {
                               <label for="form_email">Product name *</label>
                               <input
                                 id="form_email"
-                                type="email"
+                                type="text"
                                 name="email"
                                 class="form-control"
                                 placeholder="Lightweight Mobility  *"
                                 required="required"
                                 data-error="Product name is required."
+                                onChange={(e) => {
+                                  setProductName(e.target.value);
+                                }}
                               />
                             </div>
                           </div>
@@ -130,12 +213,15 @@ const ScheduleSingleMail = () => {
                               <label for="form_email">Product quantity *</label>
                               <input
                                 id="form_email"
-                                type="email"
+                                type="text"
                                 name="email"
                                 class="form-control"
                                 placeholder="5 *"
                                 required="required"
                                 data-error="Product quantity is required."
+                                onChange={(e) => {
+                                  setProductQuantity(e.target.value);
+                                }}
                               />
                             </div>
                           </div>
@@ -148,12 +234,15 @@ const ScheduleSingleMail = () => {
                               </label>
                               <input
                                 id="form_email"
-                                type="email"
+                                type="text"
                                 name="email"
                                 class="form-control"
                                 placeholder=" http://www.example.com/index.html *"
                                 required="required"
                                 data-error="Product image  is required."
+                                onChange={(e) => {
+                                  setProductUrl(e.target.value);
+                                }}
                               />
                             </div>
                           </div>
@@ -164,35 +253,16 @@ const ScheduleSingleMail = () => {
                               </label>
                               <input
                                 id="form_email"
-                                type="email"
+                                type="text"
                                 name="email"
                                 class="form-control"
                                 placeholder=" http://www.example.com/index.html *"
                                 required="required"
                                 data-error="Top banner image is required."
+                                onChange={(e) => {
+                                  setBannerUrl(e.target.value);
+                                }}
                               />
-                            </div>
-                          </div>
-                        </div>
-                        <div class="row">
-                          <div class="col-md-6">
-                            <div class="form-group">
-                              <label for="form_email">Select Date *</label>
-                              <div class="form-control">
-                                <DatePicker
-                                  className="react-datepicker__input-container  react-datepicker-popper"
-                                  selected={startDate}
-                                  onChange={(date) => setStartDate(date)}
-                                />
-                              </div>
-                            </div>
-                          </div>
-                          <div class="col-md-6">
-                            <div class="form-group">
-                              <label for="form_need">Select Time *</label>
-                              <div class="form-control">
-                                <TimePicker onChange={onChange} value={value} />
-                              </div>
                             </div>
                           </div>
                         </div>
@@ -202,6 +272,9 @@ const ScheduleSingleMail = () => {
                               type="submit"
                               class="btn btn-send  pt-2 btn-block"
                               value="Send Notification ✅"
+                              onClick={(e) => {
+                                handleButton(e);
+                              }}
                             />
                           </div>
                         </div>
